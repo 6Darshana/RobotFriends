507c46ac168dfd0f27e09afc182402192651acd5
6Darshana <dmarbate5@gmail.com>
2019-12-22 12:57:28 +0530

    Updates

---
+++ precache-manifest.63b32d10cdd7a4f3736adb94efddb38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd018982f1af36fcefabcfef8040cab6",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "1dc29a3b5b917cc10115",
+    "url": "/RobotFriends/static/css/main.c0591458.chunk.css"
+  },
+  {
+    "revision": "75fa1c9155c400242a6c",
+    "url": "/RobotFriends/static/js/2.a22a475e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RobotFriends/static/js/2.a22a475e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1dc29a3b5b917cc10115",
+    "url": "/RobotFriends/static/js/main.7b1786d4.chunk.js"
+  },
+  {
+    "revision": "a278f7c85f25ae010379",
+    "url": "/RobotFriends/static/js/runtime-main.5ab837e6.js"
+  }
+]);
